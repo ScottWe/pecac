9e6626315134a6fd5940d01efa01010ab52716eb
scottwe <scott.wesley@dal.ca>
2024-09-27 08:36:44 -0300

    Retraction to Cyclotomic.e

--- src/Pecac/Pecac/Cyclotomic.hs
+++ src/Pecac/Pecac/Cyclotomic.hs
@@ -2,12 +2,17 @@
 
 {-# LANGUAGE TemplateHaskell #-}
 
-module Pecac.Cyclotomic (cycOrder) where
+module Pecac.Cyclotomic
+  ( einv
+  , cycOrder
+  ) where
 
 -----------------------------------------------------------------------------------------
 -- * Import Section.
 
+import Data.Ratio ((%))
 import Unsafe.Coerce (unsafeCoerce)
+import Pecac.Maybe (maybeApply)
 
 import qualified Data.Complex.Cyclotomic as Cyclotomic
 import qualified Data.Map as Map
@@ -27,3 +32,40 @@ data CycInternal = CycInternal { order  :: Integer
 cycOrder :: Cyclotomic.Cyclotomic -> Integer
 cycOrder cyc = order internals
     where internals = unsafeCoerce cyc :: CycInternal
+
+-----------------------------------------------------------------------------------------
+-- * Natural Rational Log.
+
+-- | Associates a target value and the smallest primitive root of unity for which it must
+-- be a power if it were also a root of unity.
+type SearchCtx = (Cyclotomic.Cyclotomic, Cyclotomic.Cyclotomic)
+
+-- | Takes as input a search context and the current position in the search (cur). Using
+-- the primtiive root in the context (prim), this function checks iteratively if the
+-- target value can be written as prim^k * cur. If this is possible, then k is returned.
+-- Otherwise, nothing is returned. The search terminates at the smallest k such that
+-- (prim^k * cur == 1).
+einvSearch :: SearchCtx -> Cyclotomic.Cyclotomic -> Maybe Integer
+einvSearch ctx@(tar, prim) cur
+    | cur == 1   = Nothing
+    | tar == cur = Just 1
+    | otherwise  = maybeApply (einvSearch ctx $ cur * prim) (1 +)
+
+-- | If z is a root of unity,then returns the smallest possible n such that z is a power
+-- of the primitive n-th root of unity. If the cyclotomic order of z is even, then this
+-- is simply the cyclotomic order of z. Otherwise, this is twice the cyclotomic order of
+-- z, since for each odd m, the (2*m)-th roots of unity can be written as rational linear
+-- sums of the m-th roots of unity.
+einvOrder :: Cyclotomic.Cyclotomic -> Integer
+einvOrder z = if even ord then ord else ord * 2
+    where ord = cycOrder z
+
+-- | This is a partial inverse to the function Cyclotomic.e, which is defined for the
+-- group of units in the ring of integers of the universal cyclotomic field. That is, for
+-- each cyclotomic number z, if z is a root of unity, then returns (p/q) such that
+-- z = e^(i*(p/q)*2*pi). Otherwise, nothing is returned.
+einv :: Cyclotomic.Cyclotomic -> Maybe Rational
+einv 1 = Just 0
+einv z = maybeApply (einvSearch (z, prim) prim) ((%) ord)
+    where ord  = einvOrder z
+          prim = Cyclotomic.e ord
--- test/Cyclotomic-Tests.hs
+++ test/Cyclotomic-Tests.hs
@@ -4,6 +4,7 @@ import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit
 import Data.Complex.Cyclotomic
+import Data.Ratio
 import Pecac.Cyclotomic
 
 import qualified Data.Complex.Cyclotomic as Cyclotomic
@@ -18,7 +19,7 @@ cycOrderGenTestCase n =
     where val = cycOrder $ Cyclotomic.e n
           msg = "Obtained " ++ show val ++ " for: cycOrder $ Cyclotomic.e " ++ show n
 
-cycOrderGenTest 1 = cycOrderGenTestCase 1
+cycOrderGenTest 0 = pure ()
 cycOrderGenTest n = do
     cycOrderGenTestCase n
     cycOrderGenTest $ n - 1
@@ -41,9 +42,63 @@ test2 = TestCase (assertEqual "cycOrder computes the order of a sum of roots of 
           sum = c1 + c2 + c4 + c5 + c6 + c7 + c8
 
 -----------------------------------------------------------------------------------------
+-- einv
+
+einvGenTestCase n m z = assertEqual msg (Just exp) act
+    where exp = m % n
+          act = einv z
+          msg = "Obtained " ++ show act ++ " from einv instead of: " ++ show exp
+
+einvGenExpand n m z =
+    if n == m then pure () else do
+        einvGenTestCase n m $ z^m
+        einvGenExpand n (m + 1) z
+
+einvGenTest 1 = einvGenTestCase 1 0 1
+einvGenTest n = do
+    einvGenExpand n 1 $ Cyclotomic.e n
+    einvGenTest $ n - 1
+
+test3 = TestCase $ cycOrderGenTest 150
+
+test4 = TestCase (assertEqual "einv handles zero."
+                              Nothing
+                              (einv 0))
+
+test5 = TestCase (assertEqual "einv handles norm one elements of infinite order (1/2)."
+                              Nothing
+                              (einv z))
+    where z = (3 - 4 * Cyclotomic.i) / 5
+
+test6 = TestCase (assertEqual "einv handles norm one elements of infinite order (2/2)."
+                              Nothing
+                              (einv z))
+    where sqrt7 = Cyclotomic.sqrtInteger 7
+          z     = (sqrt7 * Cyclotomic.i - 3) / 4
+
+test7 = TestCase (assertEqual "einv handles elements off the unit circle (1/2)."
+                              Nothing
+                              (einv z))
+    where z = 2 * Cyclotomic.i + Cyclotomic.e 7
+
+test8 = TestCase (assertEqual "einv handles elements off the unit circle (2/2)."
+                              Nothing
+                              (einv z))
+    where e1 = Cyclotomic.e 31
+          e2 = Cyclotomic.e 12
+          e3 = Cyclotomic.e 8
+          z  = 5 * e1 - 7 * e2 + 22 * e3
+
+-----------------------------------------------------------------------------------------
 -- Orchestrates tests.
 
 tests = hUnitTestToTests $ TestList [TestLabel "cycOrder_PrimitiveRoots" test1,
-                                     TestLabel "cycOrder_SumOfRoots" test2]
+                                     TestLabel "cycOrder_SumOfRoots" test2,
+                                     TestLabel "einv_RootsOfUnity" test3,
+                                     TestLabel "einv_Zero" test4,
+                                     TestLabel "einv_IrrUnit_1" test5,
+                                     TestLabel "einv_IrrUnit_2" test6,
+                                     TestLabel "einv_NonUnit_1" test7,
+                                     TestLabel "einv_NonUnit_2" test8]
 
 main = defaultMain tests
